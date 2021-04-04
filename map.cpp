3dcf62c359c89a6228bf55448a2ba96176c77936
LZC <lizc@LZCdeMacBook-Pro.local>
2021-04-04 17:40:37 +0800

    Turn to a NB mod

--- map.cpp
+++ map.cpp
@@ -5,46 +5,30 @@ using namespace std;
 char map[400][800];
 int pos = 0;
 
-struct golds_ret {
+struct golds_ret
+{
     int x1, x2, y1, y2;
-
-}
+};
 
 // Function: print the map.
-void printMap() {
-
-}
+void printMap();
 
 // Function:
-void getPoint() {
-
-}
+void getPoint();
 
 // move the man.
-void move_man() {
-
-}
+void move_man();
 
 // move hock
-void move_hock() {
-
-}
+void move_hock();
 
 // chaeck get the gold or not
-bool check() {
-
-}
+bool check();
 
 // get input
-void getInput() {
-
-}
+void getInput();
 
 // move the gold
-void move_gold() {
+void move_gold();
 
-}
-
-bool gameover() {
-    
-}
+bool gameover();
